610d7af633af24fba8ec970f44495457516787d0
Antonio <afuenteto@gmail.com>
2026-08-18 00:55:49 +0200

    Update ProjectModal.jsx

--- src/components/ProjectModal.jsx
+++ src/components/ProjectModal.jsx
@@ -383,14 +383,7 @@ export default function ProjectModal({ proyecto, onSave, onDelete, onClose }) {
   style={{ display: 'none' }}
   onChange={subirPresupuesto}
 />         
-           <input
-  type="file"
-  accept="application/pdf"
-  id="pdfPresupuesto"
-  style={{ display: 'none' }}
-  onChange={subirPresupuesto}
-/>
-            <button type="submit" className="btn btn-primary">
+           <button type="submit" className="btn btn-primary">
               Guardar
             </button>
           </div>
