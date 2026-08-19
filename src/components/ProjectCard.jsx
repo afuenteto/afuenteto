ceb5a9f7d4ed8dc4e5a296ff1b7d94870913f5e3
Antonio <afuenteto@gmail.com>
2026-08-19 02:53:54 +0200

    Update ProjectCard.jsx

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -68,7 +68,9 @@ const totalPrevisto =
 
     <div className="card-stats" style={{ marginTop: 6 }}>
       <span className="mono">
-  <strong>{totalCobrado.toLocaleString('es-ES')} €</strong>
+  <strong>
+  {totalCobrado.toLocaleString('es-ES')} € de {valorProyecto.toLocaleString('es-ES')} €
+</strong>
   {' '}de {valorProyecto.toLocaleString('es-ES')} €
 </span>
 
