93704024cdbd3b7ae26b1d12ee3119be7b2125e4
Antonio <afuenteto@gmail.com>
2026-08-20 01:12:30 +0200

    Update SortableProjectCard.jsx

--- src/components/SortableProjectCard.jsx
+++ src/components/SortableProjectCard.jsx
@@ -1,1 +1,38 @@
+import { useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
+import ProjectCard from './ProjectCard'
 
+export default function SortableProjectCard({
+  proyecto,
+  onOpen
+}) {
+
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition
+  } = useSortable({
+    id: proyecto.id
+  })
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition
+  }
+
+  return (
+    <div
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+    >
+      <ProjectCard
+        proyecto={proyecto}
+        onOpen={onOpen}
+      />
+    </div>
+  )
+}
