d9e7152526cc4b5dd8bd3709dfcc214d3a59c776
Antonio <afuenteto@gmail.com>
2026-08-23 20:04:20 +0200

    Update StudioToday.jsx

--- src/components/StudioToday.jsx
+++ src/components/StudioToday.jsx
@@ -83,17 +83,21 @@ export default function StudioToday({
 
   <span className="today-counters">
 
-  {tareasUrgentes.length > 0 && (
-    <button
-      className="today-counter"
-      onClick={(e) => {
-        e.stopPropagation()
-        setAbierto(true)
-      }}
-    >
-      🔴 {tareasUrgentes.length}
-    </button>
-  )}
+{tareasUrgentes.length > 0 && (
+  <button
+    className="today-counter"
+    onClick={(e) => {
+      e.stopPropagation()
+
+      const primerProyecto =
+        tareasUrgentes[0].proyecto
+
+      onOpenTasks(primerProyecto)
+    }}
+  >
+    🔴 {tareasUrgentes.length}
+  </button>
+)}
 
 
   {entregas.length > 0 && (
