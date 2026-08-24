7d8131c34d55b341b6d395df37a383a8cc5296a7
Antonio <afuenteto@gmail.com>
2026-08-25 00:11:55 +0200

    Refactor BlockedPanel to handle mouse down event

--- src/components/BlockedPanel.jsx
+++ src/components/BlockedPanel.jsx
@@ -13,7 +13,14 @@ export default function BlockedPanel({
 
   return (
 
-    <div className="panel-overlay">
+    <div
+  className="panel-overlay"
+  onMouseDown={(e) => {
+    if (e.target === e.currentTarget) {
+      onClose()
+    }
+  }}
+>
 
       <div className="panel">
 
