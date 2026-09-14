c52cbf76b5826eb0ccd24613478d41c5adc87704
afuenteto <afuenteto@gmail.com>
2026-09-15 01:48:27 +0200

    Sustituir la etiqueta de idioma por el icono de traducción

--- src/components/LanguageSelector.jsx
+++ src/components/LanguageSelector.jsx
@@ -6,7 +6,9 @@ export default function LanguageSelector() {
   const language = useSyncExternalStore(subscribeLanguage, getLanguage, getLanguage)
   return (
     <div className="language-settings">
-      <label htmlFor={id} dir="auto"><span aria-hidden="true">◎</span> {translateUI('Idioma')} / Language</label>
+      <label htmlFor={id} title={`${translateUI('Idioma')} / Language`}>
+        <img src={import.meta.env.BASE_URL + 'icons/translate.png'} width="20" height="20" alt={`${translateUI('Idioma')} / Language`} draggable={false} />
+      </label>
       <select id={id} value={language} onChange={event => setLanguage(event.target.value)} dir="ltr">
         {languages.map(item => <option key={item.code} value={item.code} lang={item.code}>{item.name}</option>)}
       </select>
